e750fb9038971a20b323e07bf6fbeed861363cfd
tonistorres <sisvenda2011@gmail.com>
2022-04-22 13:55:33 -0300

    refactor: refatorando codigo implementado arquivo server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
-const app = require('express')();
+const express = require('express');
 const routes = require('./src/routes');
+const app = express();
 
 const PORT = process.env.SERVER_PORT;
-
+app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, ()=> console.log(`${PORT}`));
