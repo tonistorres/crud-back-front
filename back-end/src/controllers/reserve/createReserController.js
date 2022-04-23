a590d436c7085580581a0997c88b4e344b2e5038
tonistorres <sisvenda2011@gmail.com>
2022-04-23 18:19:19 -0300

    feat: implementando create

---
+++ back-end/src/controllers/reserve/createReserController.js
@@ -0,0 +1,12 @@
+const createReserveController = async (req, res, next) => {
+try {
+    
+    const {client, room, days, reservationDate, totalPrice } = req.body;
+
+    
+} catch (error) {
+    
+}
+}
+
+module.exports = createReserveController;
