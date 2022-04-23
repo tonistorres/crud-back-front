b224262267fa1f92f61ea258b7a2e4a40bbf045b
tonistorres <sisvenda2011@gmail.com>
2022-04-23 17:04:02 -0300

    feat: mudando o escopo da model de classe para funcao

--- back-end/src/database/models/reservation.js
+++ back-end/src/database/models/reservation.js
@@ -1,27 +1,18 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Reservation extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Reservation.init({
+
+  const Reservation = sequelize.define('Reservation', {
     client: DataTypes.STRING,
     room: DataTypes.STRING,
     days: DataTypes.INTEGER,
     reservationDate: DataTypes.DATE,
     totalPrice: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Reservation',
-  });
-  return Reservation;
-};
+
+});
+
+return Reservation;
+
+}
+
+
+
+
